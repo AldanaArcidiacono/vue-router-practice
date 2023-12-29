1c599f883bbfef61372ecff22640aabc5760df5f
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2023-12-29 09:44:32 +0100

    first commit

---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Pokemons</h1>
+  <ul>
+    <li v-for="poke of pokemons">
+      {{ poke.name }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const pokemons = ref([]);
+
+const getData = async () => {
+  try {
+    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    pokemons.value = data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => getData());
+</script>
